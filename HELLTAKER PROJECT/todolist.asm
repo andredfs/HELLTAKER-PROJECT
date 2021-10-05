7f3185d7cea832688a329ac4d2b96e4878027ed8
andredfs <42944105+andredfs@users.noreply.github.com>
2021-10-05 19:22:04 -0300

    Add files via upload

---
+++ HELLTAKER PROJECT/todolist.asm
@@ -0,0 +1,12 @@
+# [] se tem parede nao mexe
+# [] se tiver duas caixa ele nao mexe 
+# [] boneco fica parado e caixa mexe
+# [] se tiver inimgo e vago empurra o inimigo e nao se mexe
+# [] se tiver inimigo e parede ou caixa atras mata inimigo e nao se mexe
+# [] cada movimento tirar 1 do total de movimentos
+# [] RESTART - retorna ele pro comeco e volta os pontos de movimentacao
+
+
+
+EMPURRA:
+ 	
